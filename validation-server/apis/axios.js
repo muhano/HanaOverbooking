ad40afed3d8a2dd447ccc2d52f9745329aa1b905
Taufiq Ismail <taufiq.ismail@kebhana.co.id>
2022-04-12 15:17:54 +0700

    server url using dotenv

--- validation-server/apis/axios.js
+++ validation-server/apis/axios.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const instanceAxios = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: process.env.SERVER_BASE_URL
 });
 
 module.exports = instanceAxios
