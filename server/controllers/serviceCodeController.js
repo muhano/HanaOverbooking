0dabf2f473f4a323a18781ff7c3d2951906da40c
Fadly octaviano <fadlyoktaviano@gmail.com>
2022-04-20 14:24:19 +0700

    add crud for table service_code server side

--- server/controllers/serviceCodeController.js
+++ server/controllers/serviceCodeController.js
@@ -2,7 +2,7 @@ const { service_code : serviceCode } = require("../models")
 
 const getServiceCode = async (req, res, next) => {
     try {
-        const response = await service_code.findAll();
+        const response = await serviceCode.findAll();
         res.status(200).json(response)
     } catch (err) {
         next(err)
@@ -23,4 +23,63 @@ const createServiceCode = async (req, res, next) => {
     }
 }
 
-module.exports = { getServiceCode, createServiceCode } 
+const findServiceCode = async (req, res, next) => {
+    try {
+        const instanceId = req.params.id;
+
+        const response = await serviceCode.findByPk(instanceId);
+        if (!response) {
+            throw { name: "notFound" };
+        }
+        res.status(200).json(response);
+    } catch (err) {
+        next(err)
+    }
+}
+
+const editServiceCode = async (req, res, next) => {
+    try {
+        const instanceId = req.params.id
+        const {service_code, name, version, http_method, path} = req.body
+
+        const findInstance = await serviceCode.findByPk(instanceId);
+        if (!findInstance) {
+            throw { name: "notFound" };
+        }
+
+        const updatedServiceCode = await serviceCode.update(
+            {
+                service_code, name, version, http_method, path
+            },
+            {
+                where: { id: instanceId },
+                returning: true
+            }
+        );
+
+        res.status(200).json(updatedServiceCode[1][0]);
+    } catch (err) {
+        next(err)
+    }
+}
+
+const deleteServiceCode = async (req, res, next) => {
+    try {
+        const instanceId = req.params.id
+
+        const findInstance = await serviceCode.findByPk(instanceId);
+        if (!findInstance) {
+            throw { name: "notFound" };
+        }
+
+        const deletedServiceCode = await serviceCode.destroy({
+            where: { id: instanceId },
+        });
+
+        res.status(200).json({ message: `Service code with Id ${findInstance.id} deleted` });
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports = { getServiceCode, createServiceCode, findServiceCode, editServiceCode, deleteServiceCode } 
--- server/routes/serviceCodeRoute.js
+++ server/routes/serviceCodeRoute.js
@@ -3,6 +3,9 @@ const serviceCodeController = require('../controllers/serviceCodeController')
 
 router.get('/servicecode', serviceCodeController.getServiceCode)
 router.post('/servicecode', serviceCodeController.createServiceCode)
+router.get('/servicecode/:id', serviceCodeController.findServiceCode)
+router.put('/servicecode/:id', serviceCodeController.editServiceCode)
+router.delete('/servicecode/:id', serviceCodeController.deleteServiceCode)
 
 
 module.exports = router
